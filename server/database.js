6c65907fbd9401e64e6aee810827c49a0bb4af77
dkmccar2 <127516244+dkmccar2@users.noreply.github.com>
2024-10-16 19:31:05 -0400

    Add initial files

---
+++ server/database.js
@@ -0,0 +1,29 @@
+import pg from "pg";
+import env from "dotenv";
+
+const { Pool } = pg;
+env.config();
+const pool = new Pool({
+  //local variables
+  //env variables
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+// const pool = new Pool({
+//     //env variables
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PORT,
+//   });
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connection to PostgreSQL Successful");
+});
+
+export default pool;
---
+++ server/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import cors from "cors";
+
+import env from "dotenv";
+import pool from "./database.js";
+
+env.config();
+
+//const pool = require("./database.js");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors(corsOptions)); //middleware
+app.use(express.json());
+
+app.get("/getnotes", async (req, res) => {
+  console.log("<------------------------>");
+  console.log("Get route activated");
+
+  const result = await pool.query("SELECT * FROM notes");
+  if (result) {
+    console.log("Existing notes loaded from db");
+  }
+  res.send(result.rows);
+});
+
+app.post("/addnote", async (req, res) => {
+  console.log("<------------------------>");
+  console.log("Post route activated");
+
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const result = await pool.query(
+    `INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING id`,
+    [title, content]
+  );
+  const idReturn = result.rows[0].id;
+  console.log("Returning id " + result.rows[0].id);
+
+  if (result) {
+    console.log("Note added to database");
+  }
+  res.json(idReturn);
+});
+
+app.delete("/deletenote/:id", async (req, res) => {
+  console.log("<------------------------>");
+  console.log("Delete route activated");
+  const id = parseInt(req.params.id);
+  const data = req.body;
+
+  console.log("Deleting note with id: " + id);
+
+  const result = await pool.query(`DELETE FROM notes WHERE id = ${id}`);
+
+  if (result) {
+    console.log("Note successfully deleted");
+  }
+  res.json({ message: "Data received successfully", data });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}, http://localhost:${port}`);
+});
