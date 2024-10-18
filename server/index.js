3e2ebb9a61278d8cd4c93f460bf912bd4edeb15a
dkmccar2 <127516244+dkmccar2@users.noreply.github.com>
2024-10-18 05:43:28 -0400

    misc

--- server/index.js
+++ server/index.js
@@ -19,6 +19,15 @@ const port = process.env.PORT;
 
 //app.use(cors(corsOptions)); //middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", async (req, res) => {
   // const result = await pool.query("SELECT * FROM notes");
   // if (result) {
